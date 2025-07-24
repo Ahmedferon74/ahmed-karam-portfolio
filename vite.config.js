e5cb95ce1d6b8c31b0a02d492f2ebe41c03f5fbc
Ahmed Karam <ahmedferon74.com>
2025-07-24 06:00:52 +0300

    تعديل base لحل مشكلة الصفحة البيضاء بعد النشر

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: './', // ✅ المهم لحل مشكلة الصفحة البيضاء
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
